9fb283568a206653dc4c4e49789186c0a2d99496
Mike Barkmin <mike.barkmin@uni-due.de>
2021-11-29 00:00:25 +0100

    use link instead of button

--- website/src/components/FlowCard.tsx
+++ website/src/components/FlowCard.tsx
@@ -2,13 +2,12 @@ import { Flow as IFlow } from "@bitflow/core";
 import { Flow } from "@bitflow/flow";
 import {
   Box,
-  ButtonPrimary,
-  ButtonSecondary,
+  ButtonPrimaryLink,
+  ButtonSecondaryLink,
   Card,
   CardFooter,
   CardHeader,
 } from "@openpatch/patches";
-import { useRouter } from "next/router";
 
 export type FlowCardProps = {
   flow: IFlow;
@@ -16,8 +15,6 @@ export type FlowCardProps = {
 };
 
 export const FlowCard = ({ flow, id }: FlowCardProps) => {
-  const router = useRouter();
-
   return (
     <Card>
       <CardHeader>{flow.name}</CardHeader>
@@ -25,12 +22,12 @@ export const FlowCard = ({ flow, id }: FlowCardProps) => {
         <Flow {...flow} autoFitView interactive={false} />
       </Box>
       <CardFooter>
-        <ButtonPrimary fullWidth onClick={() => router.push(`/do/${id}`)}>
+        <ButtonPrimaryLink fullWidth href={`/do/${id}`}>
           Do
-        </ButtonPrimary>
-        <ButtonSecondary fullWidth onClick={() => router.push(`/editor/${id}`)}>
+        </ButtonPrimaryLink>
+        <ButtonSecondaryLink fullWidth href={`/editor/${id}`}>
           Edit
-        </ButtonSecondary>
+        </ButtonSecondaryLink>
       </CardFooter>
     </Card>
   );
